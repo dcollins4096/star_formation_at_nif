f009c044cd939c896dde7e9a1a5367519bfbbc50
dcollins <dccollins@fsu.edu>
2024-05-09 08:55:55 -0400

    Re do the last bin in the equal probability binner

--- tools/equal_probability_binner.py
+++ tools/equal_probability_binner.py
@@ -1,14 +1,33 @@
 from dtools.starter1 import *
-def equal_prob(arr,Nbins,ax=None):
+def equal_prob(arr,Nbins,ax=None,cuml=False):
     v = copy.copy(arr)
-    Npoints = v.size//Nbins
+    Ntotal = v.size
+    Npoints = np.ceil(Ntotal/(Nbins)).astype('int')
+    Remaining = Ntotal % Nbins
     v.sort()
-    ind = np.arange(0,v.size,Npoints)
-    #ind = np.concatenate([ind,v.size-1])
+    ind = np.arange(0,Ntotal,Npoints)
+    hist = ind[1:]-ind[:-1]
     edges = v[list(ind)]
-    cen = 0.5*(edges[1:]+edges[:-1])
     wid = edges[1:]-edges[:-1]
-    hist = 1/wid
+
+    #the last bin needs special handling
+    hist = np.concatenate([hist,[Remaining]])
+    last_bin_width = v[-1]-v[ind[-1]-1]
+    wid = np.concatenate([wid,[last_bin_width]])
+
+    pdf = hist/(wid*Ntotal)
+    cen = edges+0.5*wid
+
+    #print('Ntotal, Nbins, Npoints, left',v.size, Nbins, Npoints, Remaining)
+    #print('hist',hist)
+    #print('wid',wid)
+    #print('pdf',pdf)
+    #print('one?',(pdf*wid).sum())
+    #error = (pdf*wid).sum()*v.size-v.size
     if ax is not None:
-        ax.bar(cen,hist,width=wid)
+        ax.bar(cen,pdf,width=wid)
+        if cuml==True:
+            y = np.arange(v.size)/v.size
+            ax2 = ax.twinx()
+            ax2.plot(v,y,c='k')
     return hist, cen
