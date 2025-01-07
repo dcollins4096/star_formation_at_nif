143578d053d479c0a228e45ff0cd6375003c1921
dcollins <dccollins@fsu.edu>
2025-01-07 12:40:18 -0500

    tweak local brunt_tools

--- tools/brunt_tools.py
+++ tools/brunt_tools.py
@@ -6,6 +6,36 @@ import dtools.davetools as dt
 import dtools.math.power_spectrum as ps
 reload(ps)
 
+def apodize1(rho2,projax=0):
+    shape=np.array(rho2.shape)
+    baseshape=(1.*shape).astype('int')
+    base = np.zeros(baseshape)
+    start = baseshape//2-shape//2
+
+    base[start[0]:(start[0]+shape[0]), start[1]:(start[1]+shape[1])] = rho2
+
+    if 1:
+        #works pretty well.
+        x1 = np.arange(baseshape[0])
+        x2 = np.arange(baseshape[1])
+        sigma_conv=2
+        g1 = np.exp(-x1**2/(2*sigma_conv**2))**6
+        g2 = np.exp(-x2**2/(2*sigma_conv**2))**6
+        window = np.outer(g1,g2)
+
+    window/=window.sum()
+
+    a = np.fft.fftn(base)
+    b = np.fft.fftn(window)
+    c = a*b
+    rho2a = np.fft.ifftn(c)
+    q=np.abs(rho2a.imag).sum()/rho2a.imag.size
+    if q>1e-13:
+        print("!!!!!!!!!!!!!!!!!Imaginary",q)
+    return rho2a.real
+
+    
+
 
 def get_cubes(sim,frame,do_rho_4=False):
     ds = yt.load("/data/cb1/Projects/P49_EE_BB/%s/DD%04d/data%04d"%(sim, frame, frame))
@@ -22,50 +52,39 @@ def get_cubes(sim,frame,do_rho_4=False):
 
     return output
 
-def fake_powerlaw(N,alphaT,kmin,kmax,Amplitude=1,phase=False, rando=False):
-    kI = np.fft.fftfreq(N)
-    kx,ky,kz=np.meshgrid(kI,kI,kI)
-    rrr = np.sqrt(kx**2+ky**2+kz**2)
-    Ahat = np.zeros_like(rrr*1j)
-    k = np.unique(np.abs(kx))
-
-    rmin = k[kmin]
-    rmax = k[kmax]
-
-    ok = (rrr>rmin)*(rrr<=rmax)
-    ok_r=ok
-
-    #alphaT is total power. 2 makes a flat power
-    alphaV=alphaT-2 #alphaV is volume avg power.
-    alphaprime=(alphaT-2)/2
-    Ahat[ok]=Amplitude*rrr[ok]**alphaprime
-    print("Alpha prime",alphaprime)
-    #Ahat -= Ahat.min()-4
-    if rando:
-        rando=np.random.random(Ahat.size)
-        rando.shape=Ahat.shape
-        Ahat *= rando
-    Ahat[0,0,0] =100*Ahat.max()
-    #Ahat=np.ones([N,N,N])
-    if phase:
-        phi = (np.random.random(Ahat.size)-0.5)*np.pi*2
-        phi.shape = Ahat.shape
-        print('PHI',phi.min(),phi.max())
-        Ahatmag = np.abs(Ahat)
-        Ahat = Ahatmag*np.cos(phi)+Ahatmag*np.sin(phi)*1j
-        Ahat[0,0,0] = np.abs(Ahat[0,0,0])
-    H = Ahat.shape[0]//2
-    Atotal=Ahat
-    Ahat = Ahat[:,:,:H+1]
-    Q = np.fft.irfftn(Ahat)
-    return Q
-
-
-def plot_set(ftool,outname):
-    fig,ax=plt.subplots(1,2)
-    ax0=ax[0];ax1=ax[1]
-    ax0.imshow(ftool.rho2,interpolation='nearest',origin='lower')
+def plot_power(arr,ax):
+    a = np.roll(arr,arr.shape[0]//2,axis=0)
+    a = np.roll(a  ,arr.shape[1]//2,axis=1)
+    a = np.abs(a)
+    norm = mpl.colors.LogNorm(vmin=a[a>0].min(),vmax=a.max())
+    cmap = copy.copy(mpl.cm.get_cmap('viridis'))
+    cmap.set_under('w')
+    ax.imshow(a,norm=norm, cmap=cmap)
+def plot_set(ftool,outname,other=None):
+    fig,ax=plt.subplots(2,2,figsize=(10,10))
+    ax0=ax[0][0];ax1=ax[0][1]
+    ax2=ax[1][0];ax3=ax[1][1]
+    kwargs={'interpolation':'nearest', 'origin':'lower'}
+    ax0.imshow(ftool.rho2,**kwargs)
+    nhat=ftool.ps2.Nhat
+    plot_power(nhat,ax=ax2)
+    another_axis = np.mod(ftool.projax+1,3)
+    sl = [slice(None)]*3
+    sl[another_axis]=0
+    #ax3.imshow( ftool.rho.sum(0),**kwargs)
+    nhat=ftool.ps3.Nhat[tuple(sl)]
+    plot_power(nhat,ax=ax3)
     plot_brunt(ftool,ax=ax1)
+    ax0.set(title='projected density')
+    ax2.set(title='proj. FFT')
+    ax3.set(title='FFT3d[%s=0]'%'xyz'[another_axis])
+    if other is not None:
+        y=other.ps2.avgpower
+        ok = y>1e-16
+        ax1.plot(other.ps2.kcen[ok], y[ok], c='k')
+        y=other.ps3.avgpower
+        ok = y>1e-16
+        ax1.plot(other.ps3.kcen[ok], other.ps3.avgpower[ok], c='k', linestyle='--')
     fig.savefig(outname)
 def plot_brunt(ftool,outname=None, fitrange=None, method=None, ax=None):
 
@@ -86,19 +105,23 @@ def plot_brunt(ftool,outname=None, fitrange=None, method=None, ax=None):
     M3 = ftool.ps3.power>1e-16
 
     ax.plot( ftool.ps2.kcen[M1], ftool.ps2.avgpower[M1],c='m',label='P2d/V2d')
-    ax.plot( ftool.ps3.kcen[M1], ftool.ps3.avgpower[M1],c='r',label='P3d/V3d')
+    ax.plot( ftool.ps3.kcen[M3], ftool.ps3.avgpower[M3],c='r',label='P3d/V3d')
     ax.set(xscale='log',yscale='log')
     ax.legend(loc=1)
     error = 1-ftool.ratio_1
     text_x=0.05
     text_y=0.45
     dy=0.07
-    ax.text(text_x, text_y-1*dy,"%0.2e sigma_x3d"%ftool.sigma_x3d, transform=ax.transAxes)
-    ax.text(text_x, text_y-2*dy,"%0.2e sigma_x2d"%ftool.sigma_x2d, transform=ax.transAxes)
-    ax.text(text_x, text_y-3*dy,"%0.2e R "%(1./ftool.Rinv), transform=ax.transAxes)
-    ax.text(text_x, text_y-4*dy,"%0.2e sigma_B "%ftool.sigma_Brunt,  transform=ax.transAxes)
-    ax.text(text_x, text_y-5*dy,"%0.2e  error  "%error,  transform=ax.transAxes)
-    ax.text(text_x, text_y-6*dy,"%0.2e  ratio  "%(ftool.ratio_1),  transform=ax.transAxes)
+    dx = 0.3
+    ax.text(text_x,    text_y-0*dy,r"$%5.3f \sigma_{x3}$"%ftool.sigma_x3d, transform=ax.transAxes)
+    ax.text(text_x,    text_y-1*dy,r"$%5.3f \sigma_B $"%ftool.sigma_Brunt,  transform=ax.transAxes)
+    ax.text(text_x,    text_y-2*dy,r"$%5.3f  ratio $"%(ftool.ratio_1),  transform=ax.transAxes)
+    ax.text(text_x+dx, text_y-2*dy,r"$%5.3f  error $"%error,  transform=ax.transAxes)
+    ax.text(text_x,    text_y-3*dy,r"$%5.3f \sigma_{x2}$"%ftool.sigma_x2d, transform=ax.transAxes)
+    ax.text(text_x+dx, text_y-3*dy,r"$%5.3f \sigma_{2k}$"%ftool.sigma_k2d, transform=ax.transAxes)
+    ax.text(text_x,    text_y-4*dy,r"$%5.3f R $"%(1./ftool.Rinv), transform=ax.transAxes)
+    ax.text(text_x,    text_y-5*dy,r"$%5.3f \sigma_{k3}$"%ftool.sigma_k3d, transform=ax.transAxes)
+    ax.text(text_x,    text_y-6*dy,r"$%5.3f \sigma_{k2k}$"%ftool.sigma_k2dk, transform=ax.transAxes)
 
     if savefig:
         fig.savefig(outname)
@@ -136,57 +159,19 @@ class fft_tool():
         self.rho2p=None
         self.done2=False
         self.done3=False
+        self.projax=None
 
     def do3(self):
         self.ps3 = ps.powerspectrum(self.rho)
 
-    def do2(self,projax=0):
+    def do2(self,projax=0, apodize=0):
+        self.projax=projax
         if self.rho2 is None:
             self.rho2=self.rho.sum(axis=projax)
+        if apodize == 1:
+            self.rho2 = apodize1(self.rho2)
+
+
+
         self.ps2 = ps.powerspectrum(self.rho2)
-
-    def apodize1(self,projax=0):
-        self.rho2=self.rho.sum(axis=projax)
-        self.rho2/=self.rho2.shape[projax]
-        shape=np.array(self.rho2.shape)
-        baseshape=(1.*shape).astype('int')
-        base = np.zeros(baseshape)
-        start = baseshape//2-shape//2
-
-        base[start[0]:(start[0]+shape[0]), start[1]:(start[1]+shape[1])] = self.rho2
-
-        if 0:
-            #things that don't quite work
-            self.rho2 = base
-            from scipy.signal import general_gaussian
-            from scipy.signal import convolve2d
-            window = np.outer(general_gaussian(baseshape[0],6,3),general_gaussian(baseshape[0],6,3))
-            #np.roll(window,baseshape[0]//2,axis=0)
-            #np.roll(window,baseshape[0]//2,axis=1)
-        if 0:
-            #actually work ok
-            window = np.zeros(baseshape)
-            window[0:3,0:3]=1
-        if 1:
-            #works pretty well.
-            x = np.arange(baseshape[0])
-            sigma_conv=2
-            g = np.exp(-x**2/(2*sigma_conv**2))**6
-            window = np.outer(g,g)
-
-        window/=window.sum()
-        self.window=window
-        
-
-        #self.rho2 = scipy.convolve(base, window)
-        #self.rho2 = convolve2d(base, window)
-        #convolve the window function with the base
-        a = np.fft.fftn(base)
-        b = np.fft.fftn(window)
-        c = a*b
-        self.rho2 = np.fft.ifftn(c)
-        q=np.abs(self.rho2.imag).sum()/self.rho2.imag.size
-        if q>1e-13:
-            print("!!!!!!!!!!!!!!!!!Imaginary",q)
-        self.rho2=self.rho2.real
 
